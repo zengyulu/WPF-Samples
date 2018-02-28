5445a6e08f71174ad720680e865fe886b97d81a0
sizeluuu@gmail.com <sizeluuu@gmail.com>
2018-02-28 14:40:52 +0100

    Add unit test project for CalculatorDemo

---
+++ CalcDemo.UnitTests/CalcDemoUnitTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcDemo.UnitTests
+{
+    [TestClass]
+    public class CalcDemoUnitTests
+    {
+        internal const string sutFileName = "CalculatorDemo.exe";
+
+        public TestContext TestContext { get; set; }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            // leave empty
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var fi = new FileInfo(Assembly.GetExecutingAssembly().FullName);
+
+            string workingDir = fi.Directory.FullName;
+            if (!File.Exists(Path.Combine(workingDir, sutFileName)))
+                Assert.Fail("Deployment of {0} is not successful!", sutFileName);
+                    
+            Debug.WriteLine("CalcDemo.exe exists!");
+
+            ProcessStartInfo pi = new ProcessStartInfo
+            {
+                FileName = sutFileName,
+                UseShellExecute = false,
+                WorkingDirectory = workingDir,
+            };
+
+            var process = Process.Start(pi);
+            if( process == null)
+            {
+                Assert.Fail("CalcDemo.exe can't be launched! Please check this issue!");
+            }
+            Thread.Sleep(5000);
+            process.Kill();
+            Thread.Sleep(100);
+
+        }
+    }
+}
---
+++ CalcDemo.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcDemo.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Licence Owner")]
+[assembly: AssemblyProduct("CalcDemo.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Licence Owner 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("758600de-5ab1-45e5-a9a1-c1852105a98d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
